0b2756497541dba0baa8d507955d260d870fd78f
王多鱼 <xbb921@163.com>
2022-06-17 16:57:40 +0800

    1.0.2

--- dist/iview.js
+++ dist/iview.js
@@ -30902,7 +30902,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '1.0.0',
+    version: '1.0.2',
     locale: _index2.default.use,
     i18n: _index2.default.i18n,
     install: install,
